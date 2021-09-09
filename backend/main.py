d14fe298c3e38563a959968477d62665481a0708
Gokul Kumbakkara <gokulkumbakkara@gmail.com>
2021-09-09 07:03:04 +0530

    second commit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from core.config import settingobject
+from db.session import engine
+from db.base_class import Base
 
-app=FastAPI(title=settingobject.PROJECT_TITLE,version=settingobject.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine) # connects fastapi with the database engine
+def start_application():
+    app=FastAPI(title=settingobject.PROJECT_TITLE,version=settingobject.PROJECT_VERSION)
+    create_tables() 
+    return app
+
+app = start_application()
 
 @app.get("/")
 def hello_api():
