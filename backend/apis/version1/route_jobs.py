8caa026a0b783b32b4afadd5d841a15022f40f47
Gokul Kumbakkara <gokulkumbakkara@gmail.com>
2021-10-20 09:06:21 +0530

    Completed till Authorisation and Authentication

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -4,17 +4,20 @@ from fastapi import Depends,HTTPException,status
 
 from db.session import get_db
 from db.models.jobs import Job
+from db.models.users import User
 from schemas.jobs import JobCreate,ShowJob
 from db.repository.jobs import create_new_job,retrieve_job,list_jobs,update_job_by_id,delete_job_by_id
 from schemas.jobs import ShowJob
 from typing import List
+from apis.version1.route_login import get_current_user_from_token
+from db.models.users import User
 
 router = APIRouter()
 
 
 @router.post("/create-job/",response_model=ShowJob)
-def create_job(job: JobCreate,db: Session = Depends(get_db)):
-    current_user = 1
+def create_job(job: JobCreate,db: Session = Depends(get_db),current_user:User = Depends(get_current_user_from_token)):
+    current_user = current_user.id
     jobss = create_new_job(job=job,db=db,owner_id=current_user)
     return jobss
 
@@ -31,17 +34,25 @@ def retrieve_all_jobs(db:Session = Depends(get_db)):
     return jobs
 
 @router.put("/update/{id}")
-def update_job(id:int,job:JobCreate,db:Session=Depends(get_db)):
-    owner_id=1
-    message=update_job_by_id(id=id,job=job,db=db,owner_id=owner_id)
-    if not message:
+def update_job(id:int,job:JobCreate,db:Session=Depends(get_db),current_user: User = Depends(get_current_user_from_token)):
+    
+    job_retrieved = retrieve_job(id =id,db=db)
+    if not job_retrieved:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail="Job with id {id} doesn't exist")
-    return {"detail":"Successfully updated data"}
+    if job_retrieved.owner_id == current_user.id or current_user.is_superuser:
+        update_job_by_id(id=id,job=job,db=db,owner_id=current_user.id)
+        return {"detail":"Successfully updated data"}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=f"You are not permitted!!!!")
 
 @router.delete("/delete/{id}")
-def delete_job(id:int,db:Session=Depends(get_db)):
-    owner_id=1
-    message=delete_job_by_id(id=id,db=db,owner_id=owner_id)
-    if not message:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Job with id {id} doesnt exist")
-    return {"detail":"Successfully deleted data"}
+def delete_job(id: int,db: Session = Depends(get_db),current_user: User = Depends(get_current_user_from_token)):
+    job = retrieve_job(id =id,db=db)
+    if not job:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Job with {id} does not exist")
+    print(job.owner_id,current_user.id,current_user.is_superuser)
+    if job.owner_id == current_user.id or current_user.is_superuser:
+        delete_job_by_id(id=id,db=db,owner_id=current_user.id)
+        return {"msg":"Successfully deleted."}
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail=f"You are not permitted!!!!")
--- backend/apis/version1/route_login.py
+++ backend/apis/version1/route_login.py
@@ -1,8 +1,9 @@
-from fastapi.security import OAuth2PasswordRequestForm
 from fastapi import Depends,APIRouter
 from sqlalchemy.orm import Session
 from datetime import timedelta
 from fastapi import status,HTTPException
+from fastapi.security import OAuth2PasswordRequestForm,OAuth2PasswordBearer
+from jose import JWTError, jwt
 
 from db.session import get_db
 from core.hashing import Hasher
@@ -36,3 +37,26 @@ def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),db: 
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/login/token") 
+
+def get_current_user_from_token(token: str = Depends(oauth2_scheme),db: Session=Depends(get_db)): 
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, Settings.SECRET_KEY, algorithms=[Settings.ALGORITHM])
+        username: str = payload.get("sub")
+        print("username/email extracted is ",username)
+        if username is None:
+            raise credentials_exception
+            
+    except JWTError:
+        raise credentials_exception
+        
+    user = get_user(username=username,db=db)
+    if user is None:
+        raise credentials_exception
+        
+    return user
